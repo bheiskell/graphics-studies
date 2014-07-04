b7b4fb5527660ed1393e0af5cf7755cbf5ed4f93
Ben Heiskell <bheiskell@metrink.com>
2014-07-04 14:42:40 -0400

    Adding vector storage / calculations for the random samples.

--- 01-perlin-2d/perlin.js
+++ 01-perlin-2d/perlin.js
@@ -1,26 +1,15 @@
 'use strict';
 
-var Vector = function() {
-    this.x = undefined;
-    this.y = undefined;
-}
-
-/**
- * Set vector values.
- */
-Vector.prototype.set = function(x, y) {
-    this.x;
-    this.y;
-    return this;
+var Vector = function(x, y) {
+    this.x = x;
+    this.y = y;
 }
 
 /**
  * Create a vector pointing within -1 -> 1 range.
  */
 Vector.prototype.random = function() {
-    this.x = Math.random() * 2 - 1;
-    this.y = Math.random() * 2 - 1;
-    return this;
+    return new Vector(Math.random() * 2 - 1, Math.random() * 2 - 1);
 }
 
 /**
@@ -28,9 +17,26 @@ Vector.prototype.random = function() {
  */
 Vector.prototype.normalize = function() {
     var length = Math.sqrt(this.x * this.x + this.y * this.y);
-    this.x /= length;
-    this.y /= length;
-    return this;
+
+    if ( length === 0 ) {
+        return new Vector(0, 0);
+    }
+
+    return new Vector(this.x / length, this.y / length);
+}
+
+/**
+ * Calculate the dot product.
+ */
+Vector.prototype.dot = function(v) {
+    return this.x * v.x + this.y * v.y;
+}
+
+/**
+ * Subtract.
+ */
+Vector.prototype.minus = function(x, y) {
+    return new Vector(this.x - x, this.y - y);
 }
 
 var Perlin = function() {
@@ -41,7 +47,6 @@ var Perlin = function() {
 
         return xt * (1 - y) + xb * y;
     }
-
 
     this._cubic = function(x, y, tl, tr, bl, br) {
         var cubic = function(offset) {
@@ -61,6 +66,39 @@ var Perlin = function() {
         return xt * cubic(1 - y) + xb * cubic(y);
     };
 
+    this._linear_vector = function(x, y, tl, tr, bl, br) {
+        var xy = new Vector(x, y);
+        var xt = tl.dot(xy.minus(0, 0)) * (1 - x) + tr.dot(xy.minus(1, 0)) * x;
+        var xb = bl.dot(xy.minus(0, 1)) * (1 - x) + br.dot(xy.minus(1, 1)) * x;
+
+        //console.log('Offset: ', x, y);
+        //console.log('Minus: ', xy.minus(0, 0).x, xy.minus(0, 0).y);
+        //console.log('Top Left: ', tl.x, tl.y);
+        //console.log('Dot: ', tl.dot(xy.minus(0, 0)));
+        //console.log('Result: ', xt, xb, xt * (1 - y) + xb * y);
+
+        return xt * (1 - y) + xb * y;
+    };
+
+    this._cubic_vector = function(x, y, tl, tr, bl, br) {
+        var cubic = function(offset) {
+            //console.assert(offset <= 1);
+            //console.assert(offset >= 0);
+
+            var result = 3 * (offset * offset) - 2 * (offset * offset * offset);
+
+            //console.assert(result <= 1, result);
+            //console.assert(result >= 0, result);
+            return result;
+        };
+
+        var xy = new Vector(x, y);
+        var xt = tl.dot(xy.minus(0, 0)) * cubic(1 - x) + tr.dot(xy.minus(1, 0)) * cubic(x);
+        var xb = bl.dot(xy.minus(0, 1)) * cubic(1 - x) + br.dot(xy.minus(1, 1)) * cubic(x);
+
+        return xt * cubic(1 - y) + xb * cubic(y);
+    };
+
     /**
      * Interpolation of an individual point on a 2d grid given a source grid of points.
      * @param {Number[][]} source array of rows (min size 2x2)
@@ -70,7 +108,7 @@ var Perlin = function() {
      * @param {Number} height grid height
      * @param {function} interpolation function (see _linear or _cubic)
      */
-    this.interpolateSamplePoints = function(source, pointX, pointY, width, height, interpolator) {
+    this.interpolateSamples = function(source, pointX, pointY, width, height, interpolator) {
         //console.assert(source.length && source[0].length);
         //console.assert(pointX >= 0 && pointX <= width);
         //console.assert(pointY >= 0 && pointY <= height);
@@ -87,6 +125,11 @@ var Perlin = function() {
         var xCeil = Math.ceil(samplesX);
         var yCeil = Math.ceil(samplesY);
 
+        var relativeX = samplesX - xFloor;
+        var relativeY = samplesY - yFloor;
+
+        //console.log('Relative: ', relativeX, relativeY);
+
         var sampleTL = source[yFloor][xFloor];
         var sampleTR = source[yFloor][xCeil];
         var sampleBL = source[yCeil][xFloor];
@@ -94,24 +137,7 @@ var Perlin = function() {
 
         //console.log('Samples: ', sampleTL, sampleTR, sampleBL, sampleBR);
 
-        var relativeX = samplesX - xFloor;
-        var relativeY = samplesY - yFloor;
-
-        //console.log('Relative: ', relativeX, relativeY);
-
         return interpolator(relativeX, relativeY, sampleTL, sampleTR, sampleBL, sampleBR);
-    }
-
-    /**
-     * Interpolation of an individual point on a 2d grid given a source grid of vectors.
-     * @param {Vector[][]} source array of rows (min size 2x2)
-     * @param {Number} pointX the point defined within the space of the width
-     * @param {Number} pointY the point defined within the space of the height
-     * @param {Number} width grid width
-     * @param {Number} height grid height
-     * @param {function} interpolation function (see _linear or _cubic)
-     */
-    this.interpolateSampleVectors = function(source, pointX, pointY, width, height, interpolator) {
     }
 
     /**
@@ -157,15 +183,22 @@ var Perlin = function() {
      * @param {Number} minFrequency the minimum frequency
      * @param {Number} maxFrequency the maximum frequency
      * @param {function(frequency)} amplitudeCalc the function that takes a frequency and returns the amplitude to be applied
-     * @param {Boolean} truethy value to indicate if we should use linear or cubic interoplation
+     * @param {Boolean} truthy value to indicate if we should use linear or cubic interoplation
+     * @param {Boolean} truthy value to indicate if we should use vectors to represnent the samples
      * @return 2d array of points on the grid
      */
-    this.generatePerlinPoints = function(minFrequency, maxFrequency, amplitudeCalc, width, height, linear) {
-        var interpolator = linear ? this._linear : this._cubic;
+    this.generatePerlinPoints = function(minFrequency, maxFrequency, amplitudeCalc, width, height, linear, vector) {
+        var typeGenerator = vector ? this.generateSampleVectors : this.generateSamplePoints;
+
+        if (vector) {
+            var interpolator = linear ? this._linear_vector : this._cubic_vector;
+        } else {
+            var interpolator = linear ? this._linear : this._cubic;
+        }
 
         var samples = [];
         for (var frequency = minFrequency; frequency <= maxFrequency; frequency *= 2) {
-            samples.push(this.generateSamplePoints(frequency, frequency));
+            samples.push(typeGenerator(frequency, frequency));
         }
 
         var points = [];
@@ -174,7 +207,7 @@ var Perlin = function() {
             for (var x = 0; x < width; x++) {
                 for (var i = 0; i < samples.length; i++) {
                     var sample = samples[i];
-                    row[x] += amplitudeCalc(frequency) * this.interpolateSamplePoints(sample, x, y, width, height, interpolator);
+                    row[x] += amplitudeCalc(frequency) * this.interpolateSamples(sample, x, y, width, height, interpolator);
                 }
             }
             points.push(row);
